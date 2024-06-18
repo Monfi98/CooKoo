ea9c1ba4cd22e38948504d490f0fb37ef696fc45
minjung0067 <minjung0067@naver.com>
2024-06-19 03:26:14 +0900

    feat: widget UI 구현

--- CooKoo/CooKooWidget/CooKooWidgetLiveActivity.swift
+++ CooKoo/CooKooWidget/CooKooWidgetLiveActivity.swift
@@ -12,44 +12,66 @@ import SwiftUI
 // MARK: - Views
 struct LiveActivityContent: View {
     let state: TimerAttributes.ContentState
-
+    
     var body: some View {
         HStack {
-            Button {
-                //TODO: End the activity
-            } label: {
-                Label("Stop", systemImage: "stop.circle")
-                    .font(.body.bold())
-            }
-            .foregroundColor(Color.backgroundColor)
-            //.background(Material.thinMaterial)
-            .background(Color.primaryColor)
-            .clipShape(Capsule())
-            .padding(.horizontal)
-            .padding(.vertical, 8)
-            .transition(.identity)
-
+            // MARK: - 동그란 원
+//            Button {
+//                //TODO: End the activity
+//            } label: {
+//                Label("Stop", systemImage: "stop.circle")
+//                    .font(.body.bold())
+//                    .foregroundColor(Color("CooKooBlack"))
+//            }
+//            .foregroundColor(Color.backgroundColor)
+//            //.background(Material.thinMaterial)
+//            .background(Color("Background"))
+//            .clipShape(Capsule())
+//            .padding(.horizontal)
+//            .padding(.vertical, 8)
+//            .transition(.identity)
+            
+            Image("cookoo")
+                .resizable()
+                .frame(width: 100, height: 90)
+                .padding(.leading, 10)
+                .shadow(color: Color.black.opacity(0.12), radius: 6, x: 0, y: 2)
+            
             Spacer()
-
-            HStack(alignment: .center, spacing: 16) {
-                WidgetCircleTimerView(
-                    progress: state.progress,
-                    duration: state.duration
-                )
-
-                Text(state.duration)
-                    .foregroundStyle(Color.primaryColor)
-                    .font(.largeTitle.monospacedDigit())
+            VStack {
+                Text("CooKoo")
+                    .font(.headline)
                     .minimumScaleFactor(0.8)
                     .contentTransition(.numericText())
-                .animation(.spring(response: 0.2), value: state.progress)
+                    .foregroundColor(Color("AccentColor"))
+                Text(state.duration)
+                    .font(Font.system(size: 45, weight: .bold))
+                    .minimumScaleFactor(0.8)
+                    .contentTransition(.numericText())
             }
-
+            .padding(.trailing, 20)
+            
+            
+            //            HStack(alignment: .center, spacing: 16) {
+            //                WidgetCircleTimerView(
+            //                    progress: state.progress,
+            //                    duration: state.duration
+            //                )
+            //
+            //                Text(state.duration)
+            //                    .foregroundStyle(Color.primaryColor)
+            //                    .font(.largeTitle.monospacedDigit())
+            //                    .minimumScaleFactor(0.8)
+            //                    .contentTransition(.numericText())
+            //                .animation(.spring(response: 0.2), value: state.progress)
+            //            }
+            
         }
         .id(state)
         .transition(.identity)
         .padding()
-        .foregroundColor(Color.primaryColor)
+        .background(Color("Background").opacity(0.5))
+        .foregroundColor(Color("AccentColor"))
     }
 }
 
@@ -59,32 +81,29 @@ private func expandedContent(state: TimerAttributes.ContentState) -> DynamicIsla
     DynamicIslandExpandedRegion(.leading) {
         Image(systemName: "timer.circle.fill")
             .resizable()
-            .frame(width: 44.0, height: 44.0)
-            .foregroundColor(Color.primaryColor)
+            .frame(width: 80.0, height: 80.0)
+            .foregroundColor(Color("AccentColor"))
+            .padding(.vertical, 20)
+            .padding(.leading, 20)
     }
-    DynamicIslandExpandedRegion(.center) {
+    DynamicIslandExpandedRegion(.trailing) {
         VStack {
+                Text("CooKoo")
+                    .font(.headline)
+                    .minimumScaleFactor(0.8)
+                    .contentTransition(.numericText())
+                    .foregroundColor(Color("AccentColor"))
+                    .padding(.bottom, 0.5)
+
             Text(state.duration)
-                .font(.title)
+                .font(Font.system(size: 45, weight: .bold))
                 .minimumScaleFactor(0.8)
                 .contentTransition(.numericText())
-            Spacer()
-            Button {
-                //TODO: End the activity
-            } label: {
-                Label("Stop", systemImage: "stop.circle")
-                    .font(.body.bold())
-            }
-            .foregroundColor(Color.backgroundColor)
-            //.background(Material.thinMaterial)
-            .background(Color.primaryColor)
-            .clipShape(Capsule())
-            .padding(.horizontal)
-            .padding(.vertical, 8)
-            .lineLimit(1)
         }
         .id(state)
         .transition(.identity)
+        .padding(.vertical, 20)
+        .padding(.trailing, 20)
     }
 }
 
@@ -93,9 +112,9 @@ struct CooKooWidgetLiveActivity: Widget {
     var body: some WidgetConfiguration {
         ActivityConfiguration(for: TimerAttributes.self) { context in
             LiveActivityContent(state: context.state)
-                .activityBackgroundTint(Color("AccentColor").opacity(0.25))
-                .activitySystemActionForegroundColor(Color.primaryColor)
-
+                .activityBackgroundTint(Color("AccentColor"))
+                .activitySystemActionForegroundColor(Color("AccentColor"))
+            
         } dynamicIsland: { context in
             // MARK: - compact 사이즈
             DynamicIsland {
@@ -103,18 +122,18 @@ struct CooKooWidgetLiveActivity: Widget {
             } compactLeading: {
                 Image(systemName: "timer")
                     .transition(.identity)
-                    .foregroundColor(Color.primaryColor)
+                    .foregroundColor(Color("AccentColor"))
                     .padding(8)
             } compactTrailing: {
                 Text(context.state.duration)
                     .minimumScaleFactor(0.8)
                     .contentTransition(.numericText())
                     .monospacedDigit()
-                    .foregroundColor(Color.primaryColor)
+                    .foregroundColor(Color("AccentColor"))
                     .padding(8)
             } minimal: {
                 Image(systemName: "timer")
-                    .foregroundColor(Color.primaryColor)
+                    .foregroundColor(Color("AccentColor"))
             }
         }
     }
@@ -137,7 +156,7 @@ extension TimerAttributes {
 //}
 
 #Preview("Notification", as: .content, using: TimerAttributes.preview) {
-   CooKooWidgetLiveActivity()
+    CooKooWidgetLiveActivity()
 }
 contentStates: {
     TimerAttributes.ContentState(duration: "2:41", progress: 0.80)
--- CooKoo/CooKooWidget/WidgetCircleTimerView.swift
+++ CooKoo/CooKooWidget/WidgetCircleTimerView.swift
@@ -8,28 +8,25 @@
 import SwiftUI
 
 struct WidgetCircleTimerView: View {
-
+    
     var progress: Double
     var duration: String
-
+    
     var body: some View {
-        ZStack {
-            Circle()
-                .stroke(lineWidth: 5)
-                .opacity(0.25)
-                .foregroundColor(.white)
-                .frame(width: 36, height: 36)
-
-            Circle()
-                .trim(from: 0.0, to: progress)
-                .stroke(style: StrokeStyle(lineWidth: 3, lineCap: .round, lineJoin: .round))
-                .rotationEffect(.degrees(270.0))
-                .foregroundColor(Color("AccentColor"))
-                .frame(width: 36, height: 36)
+        VStack {
+            
         }
-    }
-}
+        //        ZStack {
+        //            Circle()
+        //                .trim(from: 0.0, to: progress)
+        //                .stroke(style: StrokeStyle(lineWidth: 3, lineCap: .round, lineJoin: .round))
+        //                .rotationEffect(.degrees(270.0))
+        //                .foregroundColor(Color("AccentColor"))
+        //                .frame(width: 36, height: 36)
+        //        }
+        //    }
+    }}
 
-#Preview {
-    WidgetCircleTimerView(progress: 0.8, duration: "2:41")
-}
+//#Preview {
+//    WidgetCircleTimerView(progress: 0.8, duration: "2:41")
+//}
