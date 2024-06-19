db38ff7ec4866bf87915cc14a9e9898832e3cee0
minjung0067 <minjung0067@naver.com>
2024-06-19 22:22:05 +0900

    fix: 위젯 디자인 및 noti 확장 수정

--- CooKoo/CooKooNotification/NotificationViewController.swift
+++ CooKoo/CooKooNotification/NotificationViewController.swift
@@ -11,19 +11,30 @@ import UserNotificationsUI
 
 class NotificationViewController: UIViewController, UNNotificationContentExtension {
     
-    @IBOutlet var myLabel: UILabel!
+
+    @IBOutlet weak var myLabel: UILabel!
+    //@IBOutlet weak var CooKooImageView: UIImageView!
+    
+    @IBOutlet weak var imageView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         //view.backgroundColor = UIColor(named: "AccentColor")
     }
+    
     
     func didReceive(_ notification: UNNotification) {
         // Configure the view with the notification's content
         let content = notification.request.content
-        
+
         // 예: 알림 내용을 레이블에 표시
-         //myLabel.text = content.body
+         myLabel.text = content.body
+//        imageView = UIImageView(image: "cookoo")
+        //imageView = UIImage(named: "cookoo")
+//        if let imageName = content.userInfo["image"] as? String,
+//           let image = UIImage(named: imageName) {
+//            CooKooImageView.image = image
+//        }
+        
     }
 }
