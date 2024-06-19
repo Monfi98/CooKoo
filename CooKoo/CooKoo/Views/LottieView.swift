ccc087e824a1385a310e2406f28ce2a291160f1e
minjung0067 <minjung0067@naver.com>
2024-06-19 20:19:26 +0900

    fix: lottie animation 추가

--- CooKoo/CooKoo/Views/LottieView.swift
+++ CooKoo/CooKoo/Views/LottieView.swift
@@ -6,40 +6,21 @@
 //
 
 import Lottie
-import Foundation
-import UIKit
 import SwiftUI
 
 struct LottieView: UIViewRepresentable {
-    
-    var animationFileName: String
     let loopMode: LottieLoopMode
-    var contentMode: UIView.ContentMode = .scaleAspectFit // 콘텐츠 모드 추가
-    var width: CGFloat
-    var height: CGFloat
-    
+    let jsonName: String
+
     func updateUIView(_ uiView: UIViewType, context: Context) {
-        
+
     }
-    
-    func makeUIView(context: Context) -> some UIView {
-        let view = UIView(frame: .zero)
-        
-        let animationView = LottieAnimationView(name: animationFileName)
-        animationView.loopMode = loopMode
+
+    func makeUIView(context: Context) -> Lottie.LottieAnimationView {
+        let animationView = LottieAnimationView(name: jsonName)
         animationView.play()
-        animationView.contentMode = contentMode
-        animationView.frame = CGRect(x: 0, y: 0, width: width, height: height)
-        
-        animationView.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(animationView)
-        
-        NSLayoutConstraint.activate([
-            animationView.widthAnchor.constraint(equalTo: view.widthAnchor),
-            animationView.heightAnchor.constraint(equalTo: view.heightAnchor)
-        ])
-        
-        return view
+        animationView.loopMode = loopMode
+        animationView.contentMode = .scaleAspectFit
+        return animationView
     }
 }
-
